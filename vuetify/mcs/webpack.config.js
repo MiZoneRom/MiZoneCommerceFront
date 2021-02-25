ca721ea1dcfb8c4c206efe7ab90b02ad8eae31e1
MiZone <mizonerom@hotmail.com>
2021-02-25 08:49:38 +0800

    -

---
+++ vuetify/mcs/webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      // ... 其它规则
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin()
+  ]
+}
