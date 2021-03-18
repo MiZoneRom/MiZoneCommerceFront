d7684d5d04a71c1876aa9ed1e6862c8e318f74dc
MiZone <mizonerom@hotmail.com>
2021-03-18 16:29:38 +0800

    -

--- vuetify2/src/views/manage/Navigation.vue
+++ vuetify2/src/views/manage/Navigation.vue
@@ -1,106 +1,102 @@
 <template>
-  <v-card class="ma-4">
-    <v-row class="pa-4" justify="space-between">
-      <v-col cols="4">
-        <v-treeview
-          :active.sync="active"
-          :items="navs"
-          :load-children="fetchNavs"
-          :open.sync="open"
-          activatable
-          transition
+  <v-row class="pa-4" justify="space-between">
+    <v-col cols="4" class="align-stretch">
+      <v-treeview
+        :active.sync="active"
+        :items="navs"
+        :load-children="fetchNavs"
+        :open.sync="open"
+        activatable
+        transition
+      >
+        <template v-slot:prepend="{ item }">
+          <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
+          <v-icon v-if="!item.icon">mdi-folder-network</v-icon>
+        </template>
+        <template v-slot:append="{ item }">
+          <v-btn icon>
+            <v-icon @click="createNewNav(item.id, $event)">mdi-plus</v-icon>
+          </v-btn>
+        </template>
+      </v-treeview>
+    </v-col>
+
+    <v-divider vertical></v-divider>
+
+    <v-col class="d-flex text-center">
+      <v-scroll-y-transition mode="out-in">
+        <div
+          v-if="!selectedNav"
+          class="title grey--text text--lighten-1 font-weight-light"
         >
-          <template v-slot:prepend="{ item }">
-            <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
-            <v-icon v-if="!item.icon">mdi-folder-network</v-icon>
-          </template>
-        </v-treeview>
-      </v-col>
+          选择一个导航
+        </div>
+        <v-card v-else :key="selectedNav.id" class="mx-auto" flat>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-select
+              v-model="selectedNav.parentId"
+              :items="sortNavs"
+              label="父级导航"
+              item-value="id"
+              item-text="name"
+            >
+              <template v-slot:append-item>
+                <v-divider class="mb-2"></v-divider>
+              </template>
+            </v-select>
 
-      <v-divider vertical></v-divider>
+            <v-text-field
+              v-model="selectedNav.name"
+              :counter="10"
+              :rules="nameRules"
+              label="名称"
+              required
+            ></v-text-field>
 
-      <v-col class="d-flex text-center">
-        <v-scroll-y-transition mode="out-in">
-          <div
-            v-if="!selectedNav"
-            class="title grey--text text--lighten-1 font-weight-light"
-            style="align-self: center"
-          >
-            选择一个导航
-          </div>
-          <v-card v-else :key="selectedNav.id" class="mx-auto" flat>
-            <v-form ref="form" v-model="valid" lazy-validation>
-              <v-select
-                v-model="selectedNav.parentId"
-                :items="sortNavs"
-                label="父级导航"
-                item-value="id"
-                item-text="name"
-              >
-                <template v-slot:append-item>
-                  <v-divider class="mb-2"></v-divider>
-                </template>
-              </v-select>
+            <v-text-field
+              v-model="selectedNav.subTitle"
+              :rules="nameRules"
+              label="副标题"
+              required
+            ></v-text-field>
 
-              <v-text-field
-                v-model="selectedNav.name"
-                :counter="10"
-                :rules="nameRules"
-                label="名称"
-                required
-              ></v-text-field>
+            <v-text-field
+              v-model="selectedNav.icon"
+              label="图标"
+            ></v-text-field>
 
-              <v-text-field
-                v-model="selectedNav.subTitle"
-                :rules="nameRules"
-                label="副标题"
-                required
-              ></v-text-field>
+            <v-text-field
+              v-model="selectedNav.component"
+              label="组件"
+            ></v-text-field>
 
-              <v-text-field
-                v-model="selectedNav.icon"
-                label="图标"
-              ></v-text-field>
+            <v-text-field
+              v-model="selectedNav.path"
+              label="地址"
+            ></v-text-field>
 
-              <v-text-field
-                v-model="selectedNav.component"
-                label="组件"
-              ></v-text-field>
+            <v-text-field
+              v-model="selectedNav.sortId"
+              label="排序"
+              required
+              type="number"
+            ></v-text-field>
 
-              <v-text-field
-                v-model="selectedNav.path"
-                label="地址"
-              ></v-text-field>
+            <v-textarea label="简介" v-model="selectedNav.desc"></v-textarea>
 
-              <v-text-field
-                v-model="selectedNav.sortId"
-                label="排序"
-                required
-                type="number"
-              ></v-text-field>
-
-              <v-textarea label="简介" v-model="selectedNav.desc"></v-textarea>
-
-              <v-btn
-                :disabled="!valid"
-                color="success"
-                class="mr-4"
-                @click="submitNav"
-              >
-                保存
-              </v-btn>
-            </v-form>
-          </v-card>
-        </v-scroll-y-transition>
-      </v-col>
-    </v-row>
-
-    <v-fab-transition>
-      <v-btn v-show="!edit" color="pink" dark absolute bottom right fab>
-        <v-icon>mdi-plus</v-icon>
-      </v-btn>
-    </v-fab-transition>
-  </v-card>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="submitNav"
+            >
+              保存
+            </v-btn>
+          </v-form>
+        </v-card>
+      </v-scroll-y-transition>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import apiPath from "@/service/apiPath";
@@ -170,9 +166,28 @@ export default {
       this.sortNavs.unshift({ id: 0, name: "一级导航" });
       return navData.data.data;
     },
-    submitNav() {
+    async submitNav() {
       this.$refs.form.validate();
+      var result = null;
+      if (this.selectedNav.id) {
+        result = await this.axios.put(
+          apiPath.NAVIGATION + this.selectedNav.id,
+          this.selectedNav
+        );
+      } else {
+        result = this.axios.post(apiPath.NAVIGATION, this.selectedNav);
+      }
+    },
+    createNewNav(id, event) {
+      if (event) event.cancelBubble = true;
+      var newNavData = { parentId: id, name: "新导航" };
+      this.selectedNav = newNavData;
     },
   },
 };
 </script>
+<style scoped>
+.mx-auto {
+  width: 100%;
+}
+</style>
