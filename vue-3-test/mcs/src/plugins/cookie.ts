7c905d4d24976ca37a97aec9896506831de633d7
MiZone <mizonerom@hotmail.com>
2021-07-16 17:50:54 +0800

    取消any提示

--- vue-3-test/mcs/src/plugins/cookie.ts
+++ vue-3-test/mcs/src/plugins/cookie.ts
@@ -3,20 +3,20 @@ import Cookies from 'js-cookie';
 const TokenKey = 'token'
 const RefreshTokenKey = 'refresh-token'
 
-export function getToken(): string | undefined {
-    return Cookies.get(TokenKey)
+export function getToken(): string {
+    return Cookies.get(TokenKey) || ""
 }
 
-export function setToken(token: string): string | undefined {
-    return Cookies.set(TokenKey, token)
+export function setToken(token: string): string {
+    return Cookies.set(TokenKey, token) || ""
 }
 
-export function getRefreshToken(): string | undefined {
-    return Cookies.get(RefreshTokenKey)
+export function getRefreshToken(): string {
+    return Cookies.get(RefreshTokenKey) || ""
 }
 
-export function setRefreshToken(refreshToken: string): string | undefined {
-    return Cookies.set(RefreshTokenKey, refreshToken)
+export function setRefreshToken(refreshToken: string): string {
+    return Cookies.set(RefreshTokenKey, refreshToken) || ""
 }
 
 export function removeToken(): void {
